a65054cadac173f760cbe7c16fe18cf6ca92665f
hanpham32 <hanpham3230@gmail.com>
2021-09-13 12:43:30 -0700

    Fixed button styling

--- src/components/PauseBtn.js
+++ src/components/PauseBtn.js
@@ -1,8 +1,18 @@
 import React from "react";
 const StartBtn = ({ setStatus, status }) => {
+
+  const lightTheme = {
+    backgroundColor: "#222831",
+    color: "white",
+  };
+  const darkTheme = {
+    backgroundColor: "#ffd472",
+    color: "#393E46",
+  };
+
   return (
     <div className="btn">
-      <button onClick={() => setStatus((prevStatus) => status = false)}>Pause</button>
+      <button onClick={() => setStatus((prevStatus) => status = false)} style={status ? lightTheme:darkTheme}>Pause</button>
     </div>
   );
 };
--- src/components/StartBtn.js
+++ src/components/StartBtn.js
@@ -1,9 +1,18 @@
 import React from "react";
 const StartBtn = ({setStatus, status}) => {
 
+  const lightTheme = {
+    backgroundColor: "#222831",
+    color: "white",
+  };
+  const darkTheme = {
+    backgroundColor: "#ffd472",
+    color: "#393E46",
+  };
+
   return (
-    <div className="btn">
-      <button onClick={() => setStatus(prevStatus => status = true)} >Start</button>
+    <div className="btn" >
+      <button onClick={() => setStatus(prevStatus => status = true)}  style={status ? lightTheme:darkTheme}>Start</button>
     </div>
   );
 };
